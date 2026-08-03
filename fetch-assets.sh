afdf20709c8cdb33c08c5038443ca0e6a72d45c7
Azsu-rae <aitameurmedilyas@gmail.com>
2026-08-04 00:14:25 +0300

    structuring

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+EXCLAIDRAW_IMS="https://drive.google.com/drive/folders/1so7wGZ_F_2DIYixap7ioSat0ll2mS3_S?usp=drive_link"
+LOGOS="https://drive.google.com/drive/folders/1vUyEXG6_PO8M-OU2RLf3yJmcdVjwvOkC?usp=drive_link"
+
 if ! command -v gdown > /dev/null; then
     echo "gdown is required to fetch from google drive"
     echo "Install using \`pipx install gdown\`"
 else
-    if [ ! -d ./excalidraw-ims ]; then
-        gdown --folder https://drive.google.com/drive/folders/1so7wGZ_F_2DIYixap7ioSat0ll2mS3_S?usp=drive_link
+    if [ ! -d ./courses/formation_python/excalidraw-ims ]; then
+        gdown --folder --output ./courses/formation_python/excalidraw-ims "$EXCLAIDRAW_IMS"
     fi
-    if [ ! -d ./logos ]; then
-        gdown --folder https://drive.google.com/drive/folders/1vUyEXG6_PO8M-OU2RLf3yJmcdVjwvOkC?usp=drive_link
+    if [ ! -d ./courses/formation_python/logos ]; then
+        gdown --folder --output ./courses/formation_python/logos "$LOGOS"
     fi
 fi
-
